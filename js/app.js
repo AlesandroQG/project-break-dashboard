0c5ab87ecf6fe7fc97b533167372791e40678002
Alesandro Quirós Gobbato <alesandroquirosgobbato@Mac-Studio-de-Juan.local>
2025-12-11 12:20:22 +0100

    Arreglado background

--- js/app.js
+++ js/app.js
@@ -3,12 +3,17 @@
 */
 
 const menu = document.getElementById("menu"),
-menuList = document.getElementById("menuList");
+menuList = document.getElementById("menuList"),
+isIndex = document.getElementById("is-index");
 let menuToggle = false;
 
 const setBackground = () => {
     const num = Math.floor(Math.random() * 10 + 1);
-    document.body.style.backgroundImage = `url("./media/img/bg-${num}.jpg")`;
+    if (isIndex.value === "1") {
+        document.body.style.backgroundImage = `url("./media/img/bg-${num}.jpg")`;
+    } else {
+        document.body.style.backgroundImage = `url("../media/img/bg-${num}.jpg")`;
+    }
 };
 
 setInterval(setBackground, 15000);
